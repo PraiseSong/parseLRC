4a90ee3f7761c03a00209f226e55d39bc7aa2229
praise <qiqiyoujian@gmail.com>
2013-01-30 10:27:02 +0800

    The first commit

---
+++ parseLRC.js
@@ -0,0 +1,17 @@
+function parseLRC(lyric){
+	var times = lyric.match(/\[[^\u4e00-\u9fa5|a-z|A-Z]+([^\[|\:|\]|]+)/g);
+    var html = '';
+
+    if(times){
+      for(var i=0;i<times.length;i++){
+        var pos = times[i].lastIndexOf(']');
+        var time = times[i].substr(0,pos+1);
+        time = time.replace(/[\s|\r|\t]*/ig,'');
+        html += '<li data-time="'+time+'">'+times[i].substr(pos+1)+'</li>';
+      }
+    }
+
+    var parsedResult = '<ul>'+html+'</ul>';
+
+    return parsedResult;
+}
